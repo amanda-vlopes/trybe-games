62e28c07046109230ed96518322f3de2887d9b4b
amanda-vlopes <amandavitor@hotmail.com>
2023-12-10 17:54:10 -0300

    Requisito 9 finalizado

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -53,8 +53,15 @@ public class TrybeGamesDatabase
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
     public List<StudioGamesPlayers> GetStudiosWithGamesAndPlayers()
     {
-        // Implementar
-        throw new NotImplementedException();
+        List<StudioGamesPlayers> listStudioGamesPlayers = GameStudios.Select(studio => new StudioGamesPlayers {
+            GameStudioName = studio.Name,
+            Games = Games.Where(game => game.DeveloperStudio == studio.Id)
+                    .Select(game => new GamePlayer {
+                        GameName = game.Name,
+                        Players = Players.Where(player => game.Players.Contains(player.Id)).ToList()
+                    }).ToList()
+        }).ToList();
+        return listStudioGamesPlayers;
     }
 
 }
